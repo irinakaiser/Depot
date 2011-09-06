86c19be146f137d863104e6de7e9e7533784a8bd
Irina Kaiser <kaiser.irina@googlemail.com>
2011-09-06 18:42:34 -0400

    added migrations, changed views and model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  #price_must_be_at_least_a_cent 
 end
+
+#class AddPriceToProduct < ActiveRecord::Migration
+# def self. up
+ #   add_columb :products, :price, :decimal, 
+  #    :precision => 8, :scale => 2, :default => 0
+  #  end
+  
+  #  def self. down
+  #  remove_columb :products, :price
+  #  end
+  #end
